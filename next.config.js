ea6fcbfd6eee0f96f27f4122f37f28689ae15cac
Ubuntu <ubuntu@vps-2126f287.vps.ovh.net>
2022-02-01 13:30:41 +0000

    first commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: false,
-
-  env: {
-    MONGO_URI:
-      "mongodb://baptiste:Viveleventd'hiverc'estNOEL!@localhost:27017/portfolio",
-    BASE_URL: process.env.BASE_URL,
-  },
-};
